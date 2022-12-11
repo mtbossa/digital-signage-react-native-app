11dbeac8dfc1b7f376396221588752dd180eed8d
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-12-10 23:01:44 -0300

    Working video

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, StyleSheet, Button, Text } from "react-native";
+import { Video, AVPlaybackStatus, AVPlaybackStatusSuccess, ResizeMode } from "expo-av";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const video: React.LegacyRef<Video> = React.useRef(null);
+	const [status, setStatus] = React.useState<AVPlaybackStatusSuccess | null>(null);
+	return (
+		<View style={styles.videoContainer}>
+			<Video
+				style={styles.video}
+				ref={video}
+				source={require("./assets/videos/cod_short_2.mp4")}
+				shouldPlay
+				resizeMode={ResizeMode.CONTAIN}
+				isLooping
+				onPlaybackStatusUpdate={status => setStatus(status as AVPlaybackStatusSuccess)}
+			/>
+			<View
+				style={{
+					position: "absolute",
+					top: 100,
+					left: 0,
+				}}
+			>
+				<Button
+					title={status?.isPlaying ? "Pause" : "Play"}
+					onPress={() =>
+						status?.isPlaying ? video.current?.pauseAsync() : video.current?.playAsync()
+					}
+				/>
+			</View>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+var styles = StyleSheet.create({
+	videoContainer: {
+		flex: 1,
+		backgroundColor: "red",
+	},
+	video: {
+		backgroundColor: "blue",
+		position: "absolute",
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
+	},
 });
