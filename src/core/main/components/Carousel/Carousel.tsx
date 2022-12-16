10c5b981d994395229fd4984fd48d1a08a46e5d4
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-12-15 22:55:24 -0300

    Work on Carousel

--- src/core/main/components/Carousel/Carousel.tsx
+++ src/core/main/components/Carousel/Carousel.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { Text } from "react-native";
 
+import { AppContext } from "intus-core/shared/contexts/AppContext";
+import { useSync } from "intus-core/main/hooks/useSync";
 function Carousel() {
+	const { setIsLoading } = useContext(AppContext);
+	const { sync } = useSync();
+
+	useEffect(() => {
+		(async () => {
+			setIsLoading(true);
+			await sync();
+			setIsLoading(false);
+		})();
+	}, []);
+
 	return <Text style={{ color: "white" }}>Carousel</Text>;
 }
 
