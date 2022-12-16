9c2c8bc82a4e8b5e251540fac3fbbf8edca09eb3
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-12-15 22:23:53 -0300

    Remove setIsLoading from useStorage

--- src/database/AsyncStorage/hooks/useStorage.ts
+++ src/database/AsyncStorage/hooks/useStorage.ts
@@ -5,24 +5,17 @@ import { AppContext } from "intus-core/shared/contexts/AppContext";
 import { StorageKeys } from "../StorageKeys";
 
 export const useStorage = () => {
-	const { setIsLoading } = useContext(AppContext);
-
 	const setItem = async (key: StorageKeys, value: string) => {
-		setIsLoading(true);
 		await AsyncStorage.setItem(key, value);
-		setIsLoading(false);
 	};
-	
+
 	const getItem = async (key: StorageKeys) => {
-		setIsLoading(true);
 		const item = await AsyncStorage.getItem(key);
-		setIsLoading(false);
-		
 		return item;
 	};
 
 	return {
 		setItem,
-		getItem
+		getItem,
 	};
 };
