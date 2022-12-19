c4847400ac09cad3f4544b6eeec9ea87a6ce9ac6
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-12-19 19:28:10 -0300

    Remove code

--- src/core/main/components/Carousel/Carousel.tsx
+++ src/core/main/components/Carousel/Carousel.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useContext } from "react";
 import { Button, Text } from "react-native";
-import { Q } from "@nozbe/watermelondb";
 
 import { AppContext } from "intus-core/shared/contexts/AppContext";
 import { useSync } from "intus-core/main/hooks/useSync";
 import { usePusherConnector } from "intus-core/main/hooks/usePusherConnector";
-import { database } from "intus-database/WatermelonDB";
-import { Media } from "intus-database/WatermelonDB/models/Media/Media";
+
 function Carousel() {
 	const { setIsLoading } = useContext(AppContext);
 	const { sync } = useSync();
@@ -20,18 +18,10 @@ function Carousel() {
 		})();
 	}, []);
 
-	const deletePost = async () => {
-		await database.write(async () => {
-			const [media] = await database.get<Media>("medias").query(Q.where("media_id", 10)).fetch();
-			await media.destroyPermanently();
-		});
-	};
-
 	return (
 		<>
 			<Text style={{ color: "white" }}>Carousel</Text>
 			<Button title="connect" onPress={connect} />
-			<Button title="delete" onPress={deletePost} />
 		</>
 	);
 }
