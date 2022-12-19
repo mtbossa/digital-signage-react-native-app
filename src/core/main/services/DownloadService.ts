da4ab1d720bf30e9bf9beffdf41eb35cd22321f1
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-12-19 09:57:41 -0300

    Remove Promise

--- src/core/main/services/DownloadService.ts
+++ src/core/main/services/DownloadService.ts
@@ -1,49 +1,49 @@
 import * as FileSystem from "expo-file-system";
 import { MediaWithPosts } from "intus-api/responses/DisplayPostsSyncResponse";
 
-export const downloadHandler = (media: MediaWithPosts) => {
-	return new Promise<MediaWithPosts>(async (resolve, reject) => {
-		const DOWNLOAD_URL = `http://192.168.1.99/api/media/${media.filename}/download`;
-		const DOWNLOAD_PATH = makeDownloadPath(media.filename);
-		const MAX_TRIES = 3;
+export const downloadHandler = async (
+	media: MediaWithPosts
+): Promise<MediaWithPosts & { downloadedPath: string }> => {
+	const DOWNLOAD_URL = `http://192.168.1.99/api/media/${media.filename}/download`;
+	const DOWNLOAD_PATH = makeDownloadPath(media.filename);
+	const MAX_TRIES = 3;
 
-		let tries = 1;
+	let tries = 1;
 
-		const downloadResumable = FileSystem.createDownloadResumable(
-			DOWNLOAD_URL,
-			DOWNLOAD_PATH,
-			{
-				headers: {
-					Authorization: "Bearer yZE3gVNJMtiRshEq0OeJDYesfh9jophBEMU2ij7p",
-				},
+	const downloadResumable = FileSystem.createDownloadResumable(
+		DOWNLOAD_URL,
+		DOWNLOAD_PATH,
+		{
+			headers: {
+				Authorization: "Bearer yZE3gVNJMtiRshEq0OeJDYesfh9jophBEMU2ij7p",
 			},
-			progress => {
-				console.log("Progress", progress);
-			}
-		);
-
-		while (tries <= MAX_TRIES) {
-			try {
-				tries = tries + 1;
-
-				const download = await downloadResumable.downloadAsync();
-
-				if (!isStatusCodeOk(download)) {
-					// We try to delete the file if the status code is not 200
-					await FileSystem.deleteAsync(DOWNLOAD_PATH, { idempotent: true });
-				} else {
-					resolve(media);
-					break;
-				}
-			} catch (e) {}
+		},
+		progress => {
+			console.log("Progress", progress);
 		}
+	);
 
-		reject(media);
-	});
+	while (tries <= MAX_TRIES) {
+		try {
+			tries = tries + 1;
+
+			const download = await downloadResumable.downloadAsync();
+
+			if (!isStatusCodeOk(download)) {
+				// We try to delete the file if the status code is not 200
+				await FileSystem.deleteAsync(DOWNLOAD_PATH, { idempotent: true });
+			} else {
+				return { ...media, downloadedPath: download!.uri };
+			}
+		} catch (e) {}
+	}
+
+	// throw here works as reject() call inside a Promise.
+	throw media;
 };
 
 const isStatusCodeOk = (downloadResult: FileSystem.FileSystemDownloadResult | undefined) => {
 	return downloadResult?.status === 200;
 };
 
-const makeDownloadPath = (filename: string) => FileSystem.documentDirectory + filename;
+export const makeDownloadPath = (filename: string) => FileSystem.documentDirectory + filename;
