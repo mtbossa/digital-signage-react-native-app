2d81cc94493d0ebd1d7620bc83abe4bd358560f2
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-12-20 19:53:45 -0300

    Start send ids

--- src/api/requests/DisplayPostsSyncRequest.ts
+++ src/api/requests/DisplayPostsSyncRequest.ts
@@ -1,10 +1,14 @@
 import { DisplayPostsSyncResponse } from "intus-api/responses/DisplayPostsSyncResponse";
+import { database } from "intus-database/WatermelonDB";
 import { axiosClient } from "..";
 
 export const displayPostsSyncRequest = async () => {
-	return await axiosClient.get<DisplayPostsSyncResponse>("api/display/1/posts/sync", {
-		headers: {
-			Authorization: "Bearer yZE3gVNJMtiRshEq0OeJDYesfh9jophBEMU2ij7p",
-		},
-	});
+	return await axiosClient.get<DisplayPostsSyncResponse>(
+		"api/display/1/posts/sync?postsIds[]=Saab&postsIds[]=Audi",
+		{
+			headers: {
+				Authorization: "Bearer yZE3gVNJMtiRshEq0OeJDYesfh9jophBEMU2ij7p",
+			},
+		}
+	);
 };
--- src/core/main/components/Carousel/Carousel.tsx
+++ src/core/main/components/Carousel/Carousel.tsx
@@ -4,6 +4,8 @@ import { Button, Text } from "react-native";
 import { AppContext } from "intus-core/shared/contexts/AppContext";
 import { useSync } from "intus-core/main/hooks/useSync";
 import { usePusherConnector } from "intus-core/main/hooks/usePusherConnector";
+import { database } from "intus-database/WatermelonDB";
+import { Post } from "intus-database/WatermelonDB/models/Post";
 
 function Carousel() {
 	const { setIsLoading } = useContext(AppContext);
@@ -18,10 +20,17 @@ function Carousel() {
 		})();
 	}, []);
 
+	const test = async () => {
+		const allPosts = await database.get<Post>("posts").query().fetch();
+		const allPostsIds = allPosts.map(post => `posts_ids{}=${post.post_id}`).join("&");
+		console.log(allPostsIds);
+	};
+
 	return (
 		<>
 			<Text style={{ color: "white" }}>Carousel</Text>
 			<Button title="connect" onPress={connect} />
+			<Button title="test" onPress={test} />
 		</>
 	);
 }
