435564cbbdea1eb378d3630d2892eccfb0fcf997
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-12-26 10:12:10 -0300

    Add comment

--- app.config.js
+++ app.config.js
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 
+// Reference: https://docs.expo.dev/build-reference/variables/
 let Config = {
 	apiUrl: process.env.API_URL,
 	pusherAppKey: process.env.DEVELOPMENT_PUSHER_APP_KEY,
