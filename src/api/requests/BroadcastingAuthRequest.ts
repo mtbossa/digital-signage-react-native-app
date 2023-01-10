c24af24a9f52c5032ea6c85b4df7cf4c07574cf8
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2023-01-10 08:37:16 -0300

    Remove console.log

--- src/api/requests/BroadcastingAuthRequest.ts
+++ src/api/requests/BroadcastingAuthRequest.ts
@@ -2,7 +2,6 @@ import { IntusAPIClient } from "intus-api/IntusAPIClient";
 import { BroadcastingAuthResponse } from "intus-api/responses/BroadcastingAuth";
 
 export const broadcastingAuthRequest = async (channelName: string, socketId: string) => {
-	console.log({ channelName, socketId });
 	return await IntusAPIClient.authRequest.get<BroadcastingAuthResponse>(
 		`api/broadcasting/auth?channel_name=${channelName}&socket_id=${socketId}`
 	);
