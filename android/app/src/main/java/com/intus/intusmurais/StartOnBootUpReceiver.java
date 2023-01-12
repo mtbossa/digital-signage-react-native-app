30076842057904f7ea7023e179a9d1ffd76c383b
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2023-01-12 11:30:03 -0300

    Trying to make it work

--- android/app/src/main/java/com/intus/intusmurais/StartOnBootUpReceiver.java
+++ android/app/src/main/java/com/intus/intusmurais/StartOnBootUpReceiver.java
@@ -1,2 +1,16 @@
-package com.intus.intusmurais;public class StartOnBootUpReceiver {
+package com.intus.intusmurais;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartOnBootUpReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent activityIntent = new Intent(context, MainActivity.class);
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(activityIntent);
+        }
+    }
 }
